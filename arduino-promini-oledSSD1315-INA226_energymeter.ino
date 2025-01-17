ae1d91953bc926304b8467af4f4128d4d5bafed4
uhi22 <uhi@hnng.de>
2025-01-17 11:51:22 +0100

    calculate and show Ah and Wh

--- arduino-promini-oledSSD1315-INA226_energymeter.ino
+++ arduino-promini-oledSSD1315-INA226_energymeter.ino
@@ -19,7 +19,6 @@
 
 #ifdef USE_DISPLAY
   #include <U8x8lib.h>
-
 #else
   #include <Wire.h>
 #endif
@@ -30,14 +29,33 @@
 INA226_WE ina226 = INA226_WE(I2C_ADDRESS_INA226);
 
 #ifdef USE_DISPLAY
-//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // High speed I2C
-// U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //Low spped I2C
 U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
 #endif
 
-float f;
+//float f;
 char stringBuffer[20];
 char bigBuffer[20];
+
+float shuntVoltage_mV = 0.0;
+float loadVoltage_V = 0.0;
+float busVoltage_V = 0.0;
+float current_mA = 0.0;
+float power_mW = 0.0; 
+float capacity_mAs;
+float energy_mWs;
+uint32_t lastIntegrationTime;
+
+void integrateEnergy(void) {
+  uint32_t deltaT_ms, tNow;
+  float f_mAs, f_mWs;
+  tNow = millis();
+  deltaT_ms = tNow - lastIntegrationTime;
+  f_mAs = current_mA * (float)deltaT_ms / 1000;
+  capacity_mAs += f_mAs; /* accumulate the mAs */
+  f_mWs = f_mAs * busVoltage_V;
+  energy_mWs += f_mWs; /* accumulate the energy */
+  lastIntegrationTime = tNow;
+}
 
 void setup(void) {
   Serial.begin(9600);
@@ -51,8 +69,12 @@ void setup(void) {
   u8x8.begin();
   //u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8.setFont(u8x8_font_artossans8_r);   // Schriftart auswählen
-  u8x8.drawString(1, 1, "Hello World!");
-  //delay(2000);
+  u8x8.draw2x2String(0, 0, "Hello");
+  u8x8.draw2x2String(0, 2, "World");
+  u8x8.draw2x2String(0, 4, "INA226");
+  u8x8.drawString(   0, 6, "energy meter");
+  delay(1000);
+  u8x8.clearDisplay();
 #endif
   /* Set Number of measurements for shunt and bus voltage which shall be averaged
   * Mode *     * Number of samples *
@@ -65,7 +87,15 @@ void setup(void) {
   AVERAGE_512        512
   AVERAGE_1024      1024
   */
-  ina226.setAverage(AVERAGE_16); // choose mode and uncomment for change of default
+
+/* Design decision: We choose a long averaging (512 sample) together with a fast samping (140µs). This results
+in an update-interval of 2*140µs*512=143ms. This has the following benefits:
+- gives still a fast reaction on the display
+- catches ripples quite good due to the fast sampling and long averaging.
+- has relaxed requirements regarding integrating the measured values to Ah and Wh. 100ms integration cycle is fine without data lost.
+*/
+
+  ina226.setAverage(AVERAGE_512); // choose mode and uncomment for change of default
   /* Set conversion time in microseconds
      One set of shunt and bus voltage conversion will take: 
      number of samples to be averaged x conversion time x 2
@@ -80,7 +110,7 @@ void setup(void) {
      CONV_TIME_4156       4.156 ms
      CONV_TIME_8244       8.244 ms  
   */
-  //ina226.setConversionTime(CONV_TIME_1100); //choose conversion time and uncomment for change of default
+  ina226.setConversionTime(CONV_TIME_140); //choose conversion time and uncomment for change of default
   
   /* Set measure mode
   POWER_DOWN - INA226 switched off
@@ -97,19 +127,11 @@ void setup(void) {
   
   
   
-  //ina226.waitUntilConversionCompleted(); //if you comment this line the first data might be zero  
+  //ina226.waitUntilConversionCompleted(); //if you comment this line the first data might be zero
+  lastIntegrationTime = millis();
 }
 
 void loop(void) {
-  #ifdef USE_DISPLAY
-
-  #endif
-  
-    float shuntVoltage_mV = 0.0;
-  float loadVoltage_V = 0.0;
-  float busVoltage_V = 0.0;
-  float current_mA = 0.0;
-  float power_mW = 0.0; 
   ina226.readAndClearFlags();
   shuntVoltage_mV = ina226.getShuntVoltage_mV();
   busVoltage_V = ina226.getBusVoltage_V();
@@ -130,8 +152,36 @@ void loop(void) {
   }
   Serial.println();
   #endif
-  dtostrf(busVoltage_V,6,2, stringBuffer);  sprintf(bigBuffer, "%sV", stringBuffer);   u8x8.draw2x2String(0, 0, bigBuffer);
-  dtostrf(current_mA,6,2, stringBuffer);  sprintf(bigBuffer, "%smA", stringBuffer);   u8x8.draw2x2String(0, 2, bigBuffer);
+  integrateEnergy();
+  #ifdef USE_BIG_NUMBERS
+    u8x8.drawString(0, 0, "V");
+    u8x8.drawString(0, 2, "mA");
+    u8x8.drawString(0, 4, "mW");
+    u8x8.drawString(0, 6, "mAs");
+
+    dtostrf(busVoltage_V,6,2, stringBuffer);  u8x8.draw2x2String(1, 0, stringBuffer);
+    dtostrf(current_mA,6,2, stringBuffer);    u8x8.draw2x2String(2, 2, stringBuffer);
+    dtostrf(power_mW,6,1, stringBuffer);      u8x8.draw2x2String(2, 4, stringBuffer);
+    dtostrf(capacitymAs,5,0, stringBuffer);   u8x8.draw2x2String(3, 6, stringBuffer);
+  #else
+    u8x8.drawString(0, 0, "V");
+    u8x8.drawString(0, 1, "mA");
+    u8x8.drawString(0, 2, "mW");
+    u8x8.drawString(0, 3, "mAs");
+    u8x8.drawString(0, 4, "mWs");
+    u8x8.drawString(0, 5, "Ah");
+    u8x8.drawString(0, 6, "Wh");
+
+    dtostrf(busVoltage_V,6,2, stringBuffer);  u8x8.drawString(1, 0, stringBuffer);
+    dtostrf(current_mA,6,2, stringBuffer);    u8x8.drawString(2, 1, stringBuffer);
+    dtostrf(power_mW,6,1, stringBuffer);      u8x8.drawString(2, 2, stringBuffer);
+    dtostrf(capacity_mAs,5,0, stringBuffer);  u8x8.drawString(3, 3, stringBuffer);
+    dtostrf(energy_mWs,5,0, stringBuffer);    u8x8.drawString(3, 4, stringBuffer);
+    dtostrf(capacity_mAs/3600.0/1000.0,5,2, stringBuffer);  u8x8.drawString(3, 5, stringBuffer);
+    dtostrf(energy_mWs/3600.0/1000.0,5,2, stringBuffer);    u8x8.drawString(3, 6, stringBuffer);
+    
+
+  #endif  
   //delay(3000);
-  delay(200);
+  delay(100);
 }
